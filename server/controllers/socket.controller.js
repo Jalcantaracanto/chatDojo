dcbee272e61e0efdb369fbef2c37ae9af28c3780
Jalcantaracanto <javier.alcantara.canto@gmail.com>
2023-07-05 13:27:56 -0400

    cambio

--- server/controllers/socket.controller.js
+++ server/controllers/socket.controller.js
@@ -37,31 +37,3 @@ module.exports.socketEvents = (server) => {
         io.emit('users_from_server', )
     })
 }
-
-// io.on('connection', (socket) => {
-//     socket.on('newUser', (newUserId) => {
-//         if (!activeUsers.some((user) => user.user === newUserId)) {
-//             activeUsers.push({ userId: newUserId, socketId: socket.id })
-//         }
-//         io.emit('getUsers', activeUsers)
-//     })
-
-//     socket.on('sendMessage', (data) => {
-//         const { receiverId } = data
-//         const user = activeUsers.find((user) => user.userId === receiverId)
-//         console.log('Sending from socket to:', receiverId)
-//         console.log('Data: ', data)
-//         if (user) {
-//             io.to(user.socketId).emit('receiveMessage', data)
-//         }
-//     })
-
-//     socket.on('disconnect', () => {
-//         activeUsers = activeUsers.filter((user) => user.socketId !== socket.id)
-//         io.emit('getUsers', activeUsers)
-//     })
-
-//     socket.on('newMessage', (message) => {
-//         io.emit('getMessage', message)
-//     })
-// })
